6c8d2a01e68395212fb76bc3a55cfb3fea654f07
alexandrepapas <102994168+alexandrepapas@users.noreply.github.com>
2022-07-08 10:22:39 -0300

    Ex
    Atualizando exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel= 1
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+
+if(vel>60){ //Condicao Simples !
+    console.log(`voce ultrapassou a velocidade permitida .Multado`)
+
+}
+
+console.log(`diriga sempre usando cinto de seguranca`) 
---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais= 'Brasil'
+console.log(`Vivendo em ${pais}`) 
+if  (pais == 'Brasil'){
+    console.log(" Voce e Brasileiro")
+} else {
+    
+    console.log(" Voce e estranheiro")
+
+}
+
