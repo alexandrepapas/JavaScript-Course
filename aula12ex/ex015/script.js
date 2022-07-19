fd7a760d3c0c9d1136426365ea5ddf4bb5b56939
alexandrepapas <102994168+alexandrepapas@users.noreply.github.com>
2022-07-19 15:44:51 -0300

    Criando Verificador de idade
    Atualizando

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,58 @@
-alert("ola")
+function verificar(){
+    var data= new Date()
+    var ano= data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res= document.getElementById('res')
+
+    if (fano.value.lenght ==0|| Number (fano.value) > ano){
+        window.alert("VERIFIQUE OS DADOS E TENTE NOVAMENTE")
+
+    }else{
+        var fsex= document.getElementsByName("radsex")
+        var idade= ano-Number(fano.value)
+        var genero= ""
+        var img= document.createElement("img")
+        img.setAttribute("id", "foto")
+        if(fsex[0].checked){
+            genero='homen'
+            if( idade=>0 && idade<10){
+                //criança
+                img.setAttribute('src','foto-bebe-m.png')
+            } else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'foto-jovem-m.png') 
+            } else if(idade<50){
+                //adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+          
+
+        } else if(fsex[1].checked){
+            genero= 'mulher'
+            if( idade=>0 && idade<10){
+                //criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if(idade<50){
+                //adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.innerHTML= `Detectamos ${genero} com ${idade} anos. `
+        res.appendChild(img)    
+    }
+
+   
+      
+
+
+
+}
